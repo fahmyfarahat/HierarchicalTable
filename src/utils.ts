ff7cce6125618d68c5c052c33477d223f813a07d
Fahmy <fahmy.farahat@gmail.com>
2023-07-05 14:18:13 +0200

    update utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,13 @@
 import { Column } from "./types";
 
+/**
+ * This function receives a list of Column objects as an argument and returns a new
+ * flattened array containing all Column objects in the hierarchy.
+ * It uses recursion to traverse the children of each Column and add them to the output array.
+ *
+ * @param {Column[]} columns - The initial list of Column objects, each of which may have a nested 'children' property
+ * @returns {Column[]} - A new flattened array containing all Column objects
+ */
 export const flattenColumns = (columns: Column[]): Column[] => {
   let flattenedColumns: Column[] = [];
 
@@ -17,6 +25,15 @@ export const flattenColumns = (columns: Column[]): Column[] => {
   return flattenedColumns;
 };
 
+/**
+ * This function receives a list of Column objects as an argument and returns a new
+ * array containing the ids of all collapsed Column objects in the hierarchy.
+ * It uses recursion to traverse the children of each Column and, if the Column or
+ * its parent is collapsed, adds the Column id to the output array.
+ *
+ * @param {Column[]} list - The initial list of Column objects, each of which may have a nested 'children' property
+ * @returns {string[]} - A new array containing the ids of all collapsed Column objects
+ */
 export const getCollapsedColumns = (list: Column[]): string[] => {
   let output: string[] = [];
 
